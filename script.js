ad1005c70d553e817ba3776aa5fe58ec12f4cc0c
Tronder <63967146+Tronder@users.noreply.github.com>
2020-08-23 14:12:27 +0200

    Uploading script.js - module assignment 2
    Uploading script.js - module assignment 2

---
+++ script.js
@@ -0,0 +1,64 @@
+// Question 1
+
+const backgroundButton = document.querySelector('button');
+
+const body = document.querySelector('body');
+
+backgroundButton.onclick = function () {
+  body.style.backgroundColor = 'green';
+};
+
+// Question 2
+
+const toggy = document.querySelector('.toggler');
+const toggContainer = document.querySelector('.togglee');
+
+toggy.onclick = function () {
+  if (toggContainer.classList.length < 2) {
+    toggContainer.classList.add('extra');
+  } else {
+    toggContainer.classList.remove('extra');
+  }
+};
+
+// Question 3
+
+const dropdown = document.querySelector('select');
+const paraDiv = document.querySelector('.paragraphs-container');
+
+dropdown.onchange = function () {
+  paraDiv.innerHTML = '';
+  const selectedValue = this.value;
+  console.log(selectedValue);
+
+  for (let i = 1; i <= selectedValue; i++) {
+    paraDiv.innerHTML += `<p class="content"></p>`;
+  }
+};
+
+// Question 4
+
+const text = document.querySelector('input');
+const counter = document.querySelector('.char-count b');
+
+text.onkeyup = function () {
+  const len = event.target.value.length;
+
+  counter.innerHTML = len;
+};
+
+// Question 5
+
+const main = document.querySelector('body');
+
+function scrolling() {
+  const scrolledY = window.scrollY;
+
+  if (scrolledY > 30) {
+    main.classList.add('highlight');
+  } else {
+    main.classList.remove('highlight');
+  }
+}
+
+window.onscroll = scrolling;
